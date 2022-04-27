e7788f5aa4fb4769800aeedf1d290079c03db8b1
Vinigretic <v.bakatina@gmail.com>
2022-04-27 18:27:55 +0300

    Working with files 5 - lesson_four

---
+++ pythonLessons/WorkingWithFile/lesson_four.py
@@ -0,0 +1,32 @@
+import pickle
+
+# dump(object, file) записывает обьект object в бинарный файл
+# load(file) считывает данные из бинарного файла в обьект
+
+FILENAME = 'user.dat'
+# name = input()
+# age = int(input())
+
+# with open(FILENAME, 'wb') as f:
+#     pickle.dump(name, f)
+#     pickle.dump(age, f)
+#
+# with open(FILENAME, 'rb') as f:
+#     name = pickle.load(f)
+#     age = pickle.load(f)
+#     print('Имя: ', name, 'возраст: ', age)
+
+users = [
+    ['Vika', 20, True],
+    ['Alina', 25, False],
+    ['Max', 30, False],
+    ['Dima', 35, True]
+]
+with open(FILENAME, 'wb') as f:
+    pickle.dump(users, f)
+
+with open(FILENAME, 'rb') as f:
+    a = pickle.load(f)
+    for i in a:
+        print('Имя:', i[0], '\tВозраст:', i[1], '\tЖенат(замужем):', i[2])
+
