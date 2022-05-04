0c76ada3d5611c772ffb35d19ab29ec000abcf15
Vinigretic <v.bakatina@gmail.com>
2022-05-04 16:56:56 +0300

    fix commit - dataset - practice

--- pythonLessons/WorkingWithFile/lesson_four.py
+++ pythonLessons/WorkingWithFile/lesson_four.py
@@ -3,7 +3,7 @@ import pickle
 # dump(object, file) записывает обьект object в бинарный файл
 # load(file) считывает данные из бинарного файла в обьект
 
-FILENAME = 'user.dat'
+# FILENAME = 'user.dat'
 # name = input()
 # age = int(input())
 
@@ -16,17 +16,17 @@ FILENAME = 'user.dat'
 #     age = pickle.load(f)
 #     print('Имя: ', name, 'возраст: ', age)
 
-users = [
-    ['Vika', 20, True],
-    ['Alina', 25, False],
-    ['Max', 30, False],
-    ['Dima', 35, True]
-]
-with open(FILENAME, 'wb') as f:
-    pickle.dump(users, f)
-
-with open(FILENAME, 'rb') as f:
-    a = pickle.load(f)
-    for i in a:
-        print('Имя:', i[0], '\tВозраст:', i[1], '\tЖенат(замужем):', i[2])
+# users = [
+#     ['Vika', 20, True],
+#     ['Alina', 25, False],
+#     ['Max', 30, False],
+#     ['Dima', 35, True]
+# ]
+# with open(FILENAME, 'wb') as f:
+#     pickle.dump(users, f)
+#
+# with open(FILENAME, 'rb') as f:
+#     a = pickle.load(f)
+#     for i in a:
+#         print('Имя:', i[0], '\tВозраст:', i[1], '\tЖенат(замужем):', i[2])
 
